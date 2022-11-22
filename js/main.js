3ef67c4fd5a88dbe85837d3234dc9d33beb5bf5a
NicoDevStuff <NicoG2008@gmx.de>
2022-11-22 16:25:04 +0100

    r'o[lpihurgf[hiouegruhipehi[oug
    ergio0jue-opyhgihoj[erjhi

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let count = 0;
 let button = document.getElementById("clickerButton");
 button.textContent = "Count: " + count;
 function doSomething() {
-    document.location.href = "https://github.com/Nicodevstuff";
+    document.location.href = "https://github.com/NicoDevStuff/Schulprojekt_Homepage";
 }
 function incrementCount() {
     count += 1;
