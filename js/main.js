f9b6d4bd16f44def552e5973d394b4c4fa03cf16
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2022-11-23 19:14:01 +0100

    f

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-let count = 0;
 let button = document.getElementById("clickerButton");
-button.textContent = "Count: " + count;
 function doSomething() {
     document.location.href = "https://github.com/NicoDevStuff/Schulprojekt_Homepage";
-}
-function incrementCount() {
-    count += 1;
-    button.textContent = "Count: " + count;
-}
-function getCount() {
-    return count;
 }
--- js/main.ts
+++ js/main.ts
@@ -1,17 +1,3 @@
-let count: number = 0
-let button = document.getElementById("clickerButton");
-button.textContent = "Count: " + count;
-
 function doSomething(): void {
-        document.location.href = "https://github.com/Nicodevstuff"
-}
-
-function incrementCount(): void {
-        count += 1;
-
-        button.textContent = "Count: " + count;
-}
-
-function getCount(): number {
-        return count
+        document.location.href = "https://github.com/NicoDevStuff/Schulprojekt_Homepage"
 }
